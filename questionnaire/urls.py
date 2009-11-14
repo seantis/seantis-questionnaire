75e6b39fedfb09a7413bd54c89eb2c56e6026760
Ian Ward <ian@excess.org>
2009-11-14 09:49:11 -0500

    fix for csv url

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -6,10 +6,10 @@ from views import *
 urlpatterns = patterns('',
     url(r'^$',
             questionnaire, name='questionnaire_noargs'),
+    url(r'^csv/(?P<qid>\d+)/',
+            export_csv, name='export_csv'),
     url(r'^(?P<runcode>[^/]+)/(?P<qs>\d+)/$',
             questionnaire, name='questionset'),
     url(r'^(?P<runcode>[^/]+)/',
             questionnaire, name='questionnaire'),
-    url(r'^csv/\d+/',
-            export_csv, name='export_csv')
 )
