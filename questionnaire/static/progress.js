25e85b3a80556cfcf4898e731c4aa10f4da5f1b2
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-01-30 15:43:13 +0100

    Removes console.log call from progress

--- questionnaire/static/progress.js
+++ questionnaire/static/progress.js
@@ -6,13 +6,9 @@
             dataType: 'json',
             success: function(data) {
                 var progress = data.progress;
-                console.log(progress);    
 
                 var bar = $('#progress_bar').find('.ui-progress');
                 bar.animate({'width': progress.toString() + '%'});
-            },
-            error: function(e) {
-                console.log(e);
             }
         };
 
