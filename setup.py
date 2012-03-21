f7c505009b2ff01725f22f338c10919f9d74c8b8
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-03-21 16:33:59 +0100

    Corrects version number in setup.py

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ def find_package_data(
 
 setup(
     name="seantis-questionnaire",
-    version="1.1a",
+    version="2.0",
     description="A Django application for creating online questionnaires/surveys.",
     long_description=read("README.md"),
     author="Seantis GmbH",
