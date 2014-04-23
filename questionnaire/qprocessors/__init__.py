f77421d2ced7cf4ea2542beb71d26f27b022c78d
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2014-04-23 16:10:32 +0200

    Added a NUMBER question type.
    (Uses the same code as the already implemented RANGE question type).

--- questionnaire/qprocessors/__init__.py
+++ questionnaire/qprocessors/__init__.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from questionnaire import *
 from django.utils.translation import ugettext as _
 
-import simple       # store value as returned
-import choice       # multiple choice, do checks
-import range        # range of numbers
-import timeperiod   # time periods
-import custom       # backwards compatibility support
+import simple           # store value as returned
+import choice           # multiple choice, do checks
+import range_or_number  # range of numbers
+import timeperiod       # time periods
+import custom           # backwards compatibility support
